eb04402fab9668ce123f4dea9d53b7b62eb00e3b
bye-bye-kin <21431061ms@tama.ac.jp>
2020-09-23 15:49:04 +0900

    ユーザー画面追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:name])
+  end
 end
+#デフォルトだとemail,passwordのみ受け付けるが、これでnameも新規登録することができる。
