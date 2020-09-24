fd3c1fcc941a2ff836ba76894fe9c9bd822301e2
bye-bye-kin <21431061ms@tama.ac.jp>
2020-09-24 23:30:02 +0900

    レシピ新規投稿画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
-  get 'recipes/show'
-  get 'recipes/new'
-  get 'recipes/edit'
   root to: 'home#top'
   #homeコントローラーのtopアクションが、トップページとして呼び出される。
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :user, except: [:new, :create, :destroy]
   #expect...new,create,destroy以外のルーティングを作成するということ。
+  resources :recipes
 end
