54bbd456027dce9cae10d25f9d152c36cd56c9d5
bye-bye-kin <21431061ms@tama.ac.jp>
2020-09-29 14:31:29 +0900

    delete flies

--- db/migrate/20200922062230_add_column_to_users.rb
+++ db/migrate/20200922062230_add_column_to_users.rb
@@ -1,5 +1,5 @@
 class AddColumnToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :name, :string , after: :email
+    # add_column :users, :name, :string , after: :email
   end
 end
